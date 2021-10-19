16183c3bba185904c6b5dde7a6836c869cfe1f7c
slfakani <slfakani@turing.local.isima.fr>
2021-10-19 11:20:23 +0200

    TP3

---
+++ tp_3/src/chaine.hpp
@@ -0,0 +1,58 @@
+#ifndef CHAINE_HPP
+#define CHAINE_HPP
+
+// #include <stdexcept>
+#include <exception>
+#include <string_view>
+#include <string>
+#include <sstream>
+#include <array>
+
+class ExceptionChaine : public std::exception {
+private:
+    std::string _message;
+
+public:
+    ExceptionChaine() {}
+    ExceptionChaine(std::string_view message) 
+        : _message(message) {}
+
+    const char* what() const throw () override {
+        return _message.c_str();
+    }
+};
+
+template <typename T>
+std::string chaine(T element) {
+    throw ExceptionChaine(
+        "Conversion en chaine impossible pour '" 
+        + demangle(typeid(T).name()) 
+        + "'"
+    );
+}
+
+template <>
+std::string chaine<std::string>(std::string element) {
+    return element;
+}
+
+template <>
+std::string chaine<int>(int element) {
+    return std::to_string(element);
+}
+
+template <>
+std::string chaine<double>(double element) {
+    return std::to_string(element);
+}
+
+template <typename T, typename ...Types>
+std::string chaine(const T& head, const Types& ...tail) {
+    std::stringstream ss;
+
+    ss << head << " " << chaine(tail...);
+
+    return ss.str();
+}
+
+#endif // CHAINE_HPP
